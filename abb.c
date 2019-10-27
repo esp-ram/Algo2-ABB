46d09dc8afdb9ab7c525bf8144aa96fb4a03de33
Ramiro <esperon.ramiro@gmail.com>
2019-10-27 02:25:44 -0300

    agregada abb_guardar y comparacion_rec

--- abb.c
+++ abb.c
@@ -20,6 +20,27 @@ typedef struct abb{
 }abb_t;
 
 
+
+/////////////AUXILIARES
+
+nodo_t* comparacion_rec(nodo_t* inicial, const char* nuevo, nodo_t* anterior){
+    if(inicial == NULL){
+        return anterior;
+    }
+    int resultado = arbol->comparar_clave(inicial->clave,nuevo);
+    if (resultado == 0){
+        //existe un nodo con la misma clave.
+        return inicial;
+    }else if(resultado < 0){
+        //se mueve a la izquierda
+        return comparacion_rec(inicial->hijo_izq,nuevo,inicial);
+    }else{
+        //se mueve a la derecha
+        return comparacion_rec(inicial->hijo_der,nuevo,inicial);
+    }
+}
+
+
 nodo_t* nodo_crear(char*clave, void* valor){
     nodo_t* nodo_nuevo = malloc(sizeof(nodo_t));
     if (nodo_nuevo == NULL){
@@ -41,7 +62,7 @@ nodo_t* nodo_crear(char*clave, void* valor){
 }
 
 
-
+////////////////PRIMITIVAS
 
 abb_t* abb_crear(abb_comparar_clave_t cmp, abb_destruir_dato_t destruir_dato){
     abb_t* abb_nuevo = malloc(sizeof(abb_t));
@@ -56,4 +77,32 @@ abb_t* abb_crear(abb_comparar_clave_t cmp, abb_destruir_dato_t destruir_dato){
     abb_nuevo->destruir_dato = destruir_dato;
 
     return abb_nuevo;
+}
+
+
+bool abb_guardar(abb_t *arbol, const char *clave, void *dato){
+    nodo_t* padre = comparacion_rec(arbol->raiz,clave,NULL);
+    int comparacion_padre = arbol->comparar_clave(padre->clave,clave);
+    if (padre != NULL && comparacion_padre == 0){
+        void* auxiliar = padre->valor;
+        padre->valor = dato;
+        if(arbol->destruir_dato){
+            arbol->destruir_dato(auxiliar);
+        }
+    }else{
+        nodo_t* nodo_nuevo = nodo_crear(clave,valor);
+        if(nodo_nuevo == NULL){
+            return false;
+        }
+
+        if (padre == NULL) {
+            arbol->raiz = nodo_nuevo;
+        } else if (comparacion_padre < 1) {
+            padre->hijo_izq = nodo_nuevo;
+        } else {
+            padre->hijo_der = nodo_nuevo;
+        }
+        arbol->cantidad += 1;
+    }
+    return true;
 }
